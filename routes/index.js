afa151aa0bc99d9f7304f98d227c23047fc54683
Zachariah Ngonyani <zachariahn@gmail.com>
2016-08-23 01:40:41 +0300

    define more posts api routes

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var passport        =  require('passport');
 var router          =  express.Router();
 var usersController =  require('../api/controllers/users');
 var homeController  =  require('../api/controllers/home');
+var postsController =  require('../api/controllers/posts');
 
 /* GET home page. */
 router.get('/', homeController.index);
@@ -16,5 +17,21 @@ router.post('/users/update/:id', usersController.update);
 
 // users api routes 
 router.put('/api/v1/users/update/:id', usersController.update);
+router.post('/api/v1/users/signup',    usersController.create);
+
+// posts routes 
+router.get('/posts/new',         postsController.new);
+router.post('/posts/create',     postsController.create);
+router.get('/posts',             postsController.index);
+router.get('/posts/:id',         postsController.show);
+router.get('/posts/edit/:id',    postsController.edit);
+router.post('/posts/update/:id', postsController.update);
+
+// posts api routes 
+router.get('/api/v1/posts/edit/:id',   postsController.edit);
+router.put('/api/v1/posts/update/:id', postsController.update);
+router.get('/api/v1/posts',            postsController.index);
+router.get('/api/v1/posts/:id',        postsController.show);
+router.post('/api/v1/posts/create',    postsController.create);
 
 module.exports = router;
