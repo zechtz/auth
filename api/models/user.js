3afbb168080cee40813754a800d1a9bd298c8acc
Zachariah Ngonyani <zachariahn@gmail.com>
2016-08-22 19:19:07 +0300

    Fix use model attributes

--- api/models/user.js
+++ api/models/user.js
@@ -1,12 +1,37 @@
 'use strict';
+
+var bcrypt = require('bcrypt-nodejs');
+
 module.exports =  function(sequelize, DataTypes) {
   var User     =  sequelize.define('User', {
-    username               : DataTypes.STRING,
-    email                  : DataTypes.STRING,
-    password               : DataTypes.STRING,
-    reset_password_token   : DataTypes.STRING,
-    reset_password_sent_at : DataTypes.Date
-  }, {
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          is: /^[a-z0-9\_\-]+$/i,
+        }
+      },
+      email: {
+        type: DataTypes.STRING,
+        validate: {
+          isEmail: true
+        }
+      },
+      first_name: {
+        type: DataTypes.STRING,
+      },
+      last_name: {
+        type: DataTypes.STRING,
+      },
+      password: {
+        type: DataTypes.STRING,
+      },
+      salt: {
+        type: DataTypes.STRING
+      }
+  }, 
+  {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
