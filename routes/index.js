4eb08bbcfe4b95e6ed985bed052556fdf0505cc8
Zachariah Ngonyani <zachariahn@gmail.com>
2016-08-22 19:20:22 +0300

    Create better users routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,20 @@
-var express =  require('express');
-var router  =  express.Router();
-var models  =  require('../api/models/index');
+var express         =  require('express');
+var passport        =  require('passport');
+var router          =  express.Router();
+var usersController =  require('../api/controllers/users');
+var homeController  =  require('../api/controllers/home');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', homeController.index);
+
+// users routes 
+router.get('/signup',            usersController.new);
+router.post('/signup',           usersController.create);
+router.get('/users',             usersController.index);
+router.get('/users/edit/:id',    usersController.edit);
+router.post('/users/update/:id', usersController.update);
+
+// users api routes 
+router.put('/api/v1/users/update/:id', usersController.update);
 
 module.exports = router;
