3a355c6fb17caf91507ccea35c416e2046a5745e
Zachariah Ngonyani <zachariahn@gmail.com>
2016-08-23 02:49:25 +0300

    Include passport configuration

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express      =  require('express');
+var path         =  require('path');
+var favicon      =  require('serve-favicon');
+var logger       =  require('morgan');
+var cookieParser =  require('cookie-parser');
+var bodyParser   =  require('body-parser');
 
-var routes = require('./routes/index');
+var ejs          =  require('ejs');
+var engine       =  require('ejs-mate');
 
-var app = express();
+var routes       =  require('./routes/index');
+
+var app          =  express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
@@ -21,12 +25,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+require('./config/passport');
+
 app.use(routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
+  var err    =  new Error('Not Found');
+  err.status =  404;
   next(err);
 });
 
@@ -38,8 +44,8 @@ if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
-      message: err.message,
-      error: err
+      message : err.message,
+      error   : err
     });
   });
 }
@@ -49,14 +55,14 @@ if (app.get('env') === 'development') {
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
-    message: err.message,
-    error: {}
+    message : err.message,
+    error   : {}
   });
 });
 
 // check what format we're sending 
 app.use(function (req, res, next) {
-  var format = req.param('format');
+  var format =  req.param('format');
 
   if (format) {
     req.headers.accept = 'application/' + format;
