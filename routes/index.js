e4a04b5f0b67b15a896dcdf09c759aa1cf7006a3
Zachariah Ngonyani <zachariahn@gmail.com>
2016-08-22 15:41:27 +0300

    require models inside route file

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var router = express.Router();
+var express =  require('express');
+var router  =  express.Router();
+var models  =  require('../api/models/index');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
