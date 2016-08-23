5cf0d6940321aa96e806bf98f034539c23cde887
Zachariah Ngonyani <zachariahn@gmail.com>
2016-08-23 12:35:01 +0300

    Properly format posts controller code

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -24,55 +24,55 @@ module.exports.show  = function(req, res){
   });
 };
 
-  // New function renders the new post form 
-  module.exports.new =  function(req, res){
-    res.render('posts/new');
-  };
+// New function renders the new post form 
+module.exports.new =  function(req, res){
+  res.render('posts/new');
+};
 
-  // Create function creates & saves the post 
-  module.exports.create =  function(req, res){
+// Create function creates & saves the post 
+module.exports.create =  function(req, res){
 
-    var title           =  req.body.title;
-    var UserId          =  req.body.UserId;
-    var content         =  req.body.content;
+  var title           =  req.body.title;
+  var UserId          =  req.body.UserId;
+  var content         =  req.body.content;
 
-    models.Post.create({
-        title   : title,
-        UserId  : UserId,
-        content : content
-    }).then(function(post){
-    res.json(post);
+  models.Post.create({
+      title   : title,
+      UserId  : UserId,
+      content : content
+  }).then(function(post){
+  res.json(post);
+  });
+};
+
+// Edit post renders the edit post form 
+module.exports.edit = function(req, res){
+  models.Post.find({ id : req.params.id }, function (err, post){
+    if(err) return err;
+    res.render('posts/edit', {
+        post  : post,
+        title : "Edit"
     });
-    };
+  });
+};
 
-    // Edit post renders the edit post form 
-    module.exports.edit = function(req, res){
-      models.Post.find({ id : req.params.id }, function (err, post){
-        if(err) return err;
-        res.render('posts/edit', {
-            post  : post,
-            title : "Edit"
-        });
-      });
-      };
-
-      //update post updates posts attributes and save to database
-      module.exports.update = function(req, res){
-        models.Post.find({
-            where: {
-              id: req.params.id 
-            }
+  //update post updates posts attributes and save to database
+module.exports.update = function(req, res){
+    models.Post.find({
+        where: {
+          id: req.params.id 
+        }
+    })
+    .then(function(post){
+      if (post){
+        post.updateAttributes({
+            title   : req.body.title,
+            UserId  : req.body.UserId,
+            content : req.body.content
         })
         .then(function(post){
-          if (post){
-            post.updateAttributes({
-                title   : req.body.title,
-                UserId  : req.body.UserId,
-                content : req.body.content
-            })
-            .then(function(post){
-              res.json(post);
-            })
-          }
-        });
-        };
+          res.json(post);
+        })
+      }
+    });
+};
