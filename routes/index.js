e534f18516bc1416f8d82674487306eda55012bf
Zachariah Ngonyani <zachariahn@gmail.com>
2016-08-23 17:10:19 +0300

    Add isAuthenticated helper function in routes index

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,14 @@ var homeController    =  require('../api/controllers/home');
 var postsController   =  require('../api/controllers/posts');
 var sessionController =  require('../api/controllers/session');
 
+/* Helper functions */
+var isAuthenticated = function (req, res, next) {
+  if (req.isAuthenticated())
+    return next()
+  req.flash('error', 'You have to be logged in to access the page.')
+  res.redirect('/login')
+}
+
 /* GET home page. */
 router.get('/', homeController.index);
 
@@ -23,14 +31,14 @@ router.post('/api/v1/signup',          usersController.create);
 // sessions routes 
 router.get('/login',           sessionController.new);
 router.post('/session/create', sessionController.create);
-router.get('/logout',         sessionController.destroy);
+router.get('/logout',          sessionController.destroy);
 
 // posts routes 
 router.get('/posts/new',         postsController.new);
 router.post('/posts/create',     postsController.create);
 router.get('/posts',             postsController.index);
 router.get('/posts/:id',         postsController.show);
-router.get('/posts/edit/:id',    postsController.edit);
+router.get('/posts/edit/:id',    isAuthenticated, postsController.edit);
 router.post('/posts/update/:id', postsController.update);
 
 // posts api routes 
@@ -38,6 +46,6 @@ router.get('/api/v1/posts/edit/:id',   postsController.edit);
 router.put('/api/v1/posts/update/:id', postsController.update);
 router.get('/api/v1/posts',            postsController.index);
 router.get('/api/v1/posts/:id',        postsController.show);
-router.post('/api/v1/posts/create',    postsController.create);
+router.put('/api/v1/posts/create',     postsController.create);
 
 module.exports = router;
