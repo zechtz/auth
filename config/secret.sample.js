f1460485667e268efbe5f24bc157cbf0514339ff
Zachariah Ngonyani <zachariahn@gmail.com>
2016-08-23 12:18:09 +0300

    Add a sample secret config file

---
+++ config/secret.sample.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+  DB_URL: 'databasesystem://yourdatabaseusername:password@localhost/yourdatabasename',
+  secretKey: 'yoursecretkeyshouldbehere'
+}
+
+/* databasesystem eg postgres, mysql */
